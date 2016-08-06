42760fcc265b085b90f2e593d231350da4a1315d
ninjawarrior1337 <t.rothled@gmail.com>
2016-08-05 22:02:35 -0700

    made abos great again

--- src/main/java/com/ninjawarrior1337/abos/RegisterRecipes.java
+++ src/main/java/com/ninjawarrior1337/abos/RegisterRecipes.java
@@ -1,48 +1,36 @@
 package com.ninjawarrior1337.abos;
 
 import com.mrcrayfish.furniture.api.IRecipeRegistry;
+import com.mrcrayfish.furniture.api.RecipeType;
 import com.mrcrayfish.furniture.api.RecipeVariables;
+import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 
 
 public class RegisterRecipes
 {
-    public static void registerCustomRecipes(IRecipeRegistry registry)
+    public static void register(IRecipeRegistry registry)
     {
         RecipeVariables freezer = new RecipeVariables();
-        freezer.addValue("input", new ItemStack(Items.blaze_rod));
-        freezer.addValue("output", new ItemStack(abos.itemObsidianRod));
-        registry.registerRecipe("freezer", freezer);
+        freezer.setInput(new ItemStack(Items.blaze_rod));
+        freezer.setOutput(new ItemStack(abos.itemObsidianRod));
+        registry.registerRecipe(RecipeType.FREEZER, freezer);
+        freezer.setInput(new ItemStack(Items.rotten_flesh));
+        freezer.setOutput(new ItemStack(Items.leather));
+        registry.registerRecipe(RecipeType.FREEZER, freezer);
 
         RecipeVariables microwave = new RecipeVariables();
-        microwave.addValue("input", new ItemStack(abos.itemObsidianRod));
-        microwave.addValue("output", new ItemStack(Items.blaze_rod));
-        registry.registerRecipe("microwave", microwave);
+        microwave.setInput(new ItemStack(abos.itemObsidianRod));
+        microwave.setOutput(new ItemStack(Items.blaze_rod));
+        registry.registerRecipe(RecipeType.MICROWAVE, microwave);
 
-        RecipeVariables minebay = new RecipeVariables();
-        minebay.addValue("input", new ItemStack(abos.itemObsidianRod));
-        minebay.addValue("currency", new ItemStack(Items.emerald));
-        minebay.addValue("price", 7);
-        registry.registerRecipe("minebay", minebay);
-
-        RecipeVariables dishwasher = new RecipeVariables();
-        dishwasher.addValue("input", new ItemStack(abos.diamondObsidianAxe));
-        registry.registerRecipe("dishwasher", dishwasher);
-        dishwasher.addValue("input", new ItemStack(abos.diamondObsidianPickaxe));
-        registry.registerRecipe("dishwasher", dishwasher);
-        dishwasher.addValue("input", new ItemStack(abos.diamondObsidianSword));
-        registry.registerRecipe("dishwasher", dishwasher);
-        dishwasher.addValue("input", new ItemStack(abos.diamondObsidianShovel));
-        registry.registerRecipe("dishwasher", dishwasher);
-        dishwasher.addValue("input", new ItemStack(abos.diamondObsidianHoe));
-        registry.registerRecipe("dishwasher", dishwasher);
-
-        RecipeVariables choppingboard = new RecipeVariables();
-        choppingboard.addValue("input", new ItemStack(Items.reeds));
-        choppingboard.addValue("output", new ItemStack(abos.scrod, 7));
-        registry.registerRecipe("choppingboard", choppingboard);
-
+        RecipeVariables blender = new RecipeVariables();
+        blender.setName("Derek Juice");
+        blender.setHeal(20);
+        blender.setIngredients(new ItemStack(Blocks.obsidian), new ItemStack(Blocks.tnt));
+        blender.setColour(new int[]{162, 0, 255});
+        registry.registerRecipe(RecipeType.BLENDER, blender);
 
     }
 }
