5e3ffee0198f2c04891c932737f76c56447379f3
ninjawarrior1337 <t.rothled@gmail.com>
2016-08-06 18:03:01 -0700

    Made CFM api work again

--- src/main/java/com/ninjawarrior1337/abos/RegisterRecipes.java
+++ src/main/java/com/ninjawarrior1337/abos/RegisterRecipes.java
@@ -1,7 +1,6 @@
 package com.ninjawarrior1337.abos;
 
 import com.mrcrayfish.furniture.api.IRecipeRegistry;
-import com.mrcrayfish.furniture.api.RecipeType;
 import com.mrcrayfish.furniture.api.RecipeVariables;
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
@@ -13,24 +12,24 @@ public class RegisterRecipes
     public static void register(IRecipeRegistry registry)
     {
         RecipeVariables freezer = new RecipeVariables();
-        freezer.setInput(new ItemStack(Items.blaze_rod));
-        freezer.setOutput(new ItemStack(abos.itemObsidianRod));
-        registry.registerRecipe(RecipeType.FREEZER, freezer);
-        freezer.setInput(new ItemStack(Items.rotten_flesh));
-        freezer.setOutput(new ItemStack(Items.leather));
-        registry.registerRecipe(RecipeType.FREEZER, freezer);
+        freezer.addValue("input", new ItemStack(Items.blaze_rod));
+        freezer.addValue("output", new ItemStack(abos.itemObsidianRod));
+        registry.registerRecipe("freezer", freezer);
+        freezer.addValue("input", new ItemStack(Items.rotten_flesh));
+        freezer.addValue("output", new ItemStack(Items.leather));
+        registry.registerRecipe("freezer", freezer);
 
         RecipeVariables microwave = new RecipeVariables();
-        microwave.setInput(new ItemStack(abos.itemObsidianRod));
-        microwave.setOutput(new ItemStack(Items.blaze_rod));
-        registry.registerRecipe(RecipeType.MICROWAVE, microwave);
+        microwave.addValue("input", new ItemStack(abos.itemObsidianRod));
+        microwave.addValue("output", new ItemStack(Items.blaze_rod));
+        registry.registerRecipe("microwave", microwave);
 
         RecipeVariables blender = new RecipeVariables();
-        blender.setName("Derek Juice");
-        blender.setHeal(20);
-        blender.setIngredients(new ItemStack(Blocks.obsidian), new ItemStack(Blocks.tnt));
-        blender.setColour(new int[]{162, 0, 255});
-        registry.registerRecipe(RecipeType.BLENDER, blender);
+        blender.addValue("name", "Derek Juice");
+        blender.addValue("heal", 20);
+        blender.addValue("ingredients", new ItemStack[]{new ItemStack(Blocks.tnt, 1, 0), new ItemStack(Blocks.obsidian, 1 ,0)});
+        blender.addValue("colour", new int[]{162, 0, 255});
+        registry.registerRecipe("blender", blender);
 
     }
 }
