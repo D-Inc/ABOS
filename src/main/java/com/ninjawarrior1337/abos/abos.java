b6251bed1b19fb76dcc8e9354ff7f67e23722a73
ninjawarrior1337 <t.rothled@gmail.com>
2016-08-11 20:08:08 -0700

    added recipe api to git

--- src/main/java/com/ninjawarrior1337/abos/abos.java
+++ src/main/java/com/ninjawarrior1337/abos/abos.java
@@ -33,9 +33,8 @@ public class abos {
     public static Item scHoe;
     public static Item scShovel;
     public static Item scApple;
-    public static Item DirtCell;
     public static Block oreChicken;
-    public static Item TestCell;
+    public static Item DirtCell;
 
 
 
@@ -68,11 +67,8 @@ public class abos {
         scrod = new scrod().setUnlocalizedName("scrod").setMaxStackSize(64).setTextureName("abos:scrod").setMaxStackSize(64);
         GameRegistry.registerItem(scrod, scrod.getUnlocalizedName().substring(5));
 
-        //DirtCell = new DirtCell();
-        //GameRegistry.registerItem(DirtCell, DirtCell.getUnlocalizedName().substring(5));
-
-        TestCell = new TestCell();
-        GameRegistry.registerItem(TestCell, TestCell.getUnlocalizedName().substring(5));
+        DirtCell = new DirtCell();
+        GameRegistry.registerItem(DirtCell, DirtCell.getUnlocalizedName().substring(5));
 
         //Tools
         diamondObsidianPickaxe = new ItemObsidianPickaxe(diamondObsidianToolMaterial).setUnlocalizedName("ItemObsidianPickaxe").setTextureName("abos:obPick");
--- src/main/java/com/ninjawarrior1337/abos/item/DirtCell.java
+++ src/main/java/com/ninjawarrior1337/abos/item/DirtCell.java
@@ -1,119 +1,245 @@
 package com.ninjawarrior1337.abos.item;
 
-import appeng.api.config.AccessRestriction;
+import appeng.api.AEApi;
 import appeng.api.config.FuzzyMode;
-import appeng.api.implementations.items.IAEItemPowerStorage;
+import appeng.api.config.IncludeExclude;
+import appeng.api.exceptions.MissingDefinition;
 import appeng.api.implementations.items.IStorageCell;
+import appeng.api.implementations.items.IUpgradeModule;
+import appeng.api.storage.ICellInventory;
+import appeng.api.storage.ICellInventoryHandler;
+import appeng.api.storage.IMEInventoryHandler;
+import appeng.api.storage.StorageChannel;
 import appeng.api.storage.data.IAEItemStack;
-import com.ninjawarrior1337.abos.Reference;
+import appeng.api.storage.data.IItemList;
+import appeng.core.localization.GuiText;
+import appeng.items.AEBaseItem;
+import appeng.items.contents.CellConfig;
+import appeng.items.contents.CellUpgrades;
+import appeng.util.InventoryAdaptor;
+import appeng.util.Platform;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraft.inventory.IInventory;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
+import net.minecraftforge.common.util.ForgeDirection;
+
+import java.util.List;
 
 /**
- * Created by ninjawarrior1337 on 8/11/16.
+ * Created by Rocker545 on 8/11/2016.
  */
-public class DirtCell extends Item implements IStorageCell, IAEItemPowerStorage
-{
-    public static final String suffix = "8";
-
-    public static final int byte_cell = 8;
-    public static final int type_cell = 1;
-    private final int MAX_POWER = 32000;
+public class DirtCell extends AEBaseItem implements IStorageCell{
+    private final int totalBytes = 16384;
+    private final int perType = 8;
+    private final double idleDrain = 0;
 
     public DirtCell()
     {
+        this.setMaxStackSize(1);
         setUnlocalizedName("dirt_cell");
-        setTextureName(Reference.RESOURCE_PREFIX+"dirt_cell");
     }
 
     @Override
-    public int getBytes(ItemStack cellItem) {
+    public void addCheckedInformation(final ItemStack stack, final EntityPlayer player, final List<String> lines, final boolean displayMoreInfo )
+    {
+        final IMEInventoryHandler<?> inventory = AEApi.instance().registries().cell().getCellInventory( stack, null, StorageChannel.ITEMS );
+
+        if( inventory instanceof ICellInventoryHandler)
+        {
+            final ICellInventoryHandler handler = (ICellInventoryHandler) inventory;
+            final ICellInventory cellInventory = handler.getCellInv();
+
+            if( cellInventory != null )
+            {
+                lines.add( cellInventory.getUsedBytes() + " " + GuiText.Of.getLocal() + ' ' + cellInventory.getTotalBytes() + ' ' + GuiText.BytesUsed.getLocal() );
+
+                lines.add( cellInventory.getStoredItemTypes() + " " + GuiText.Of.getLocal() + ' ' + cellInventory.getTotalItemTypes() + ' ' + GuiText.Types.getLocal() );
+
+                if( handler.isPreformatted() )
+                {
+                    final String list = ( handler.getIncludeExcludeMode() == IncludeExclude.WHITELIST ? GuiText.Included : GuiText.Excluded ).getLocal();
+
+                    if( handler.isFuzzy() )
+                    {
+                        lines.add( GuiText.Partitioned.getLocal() + " - " + list + ' ' + GuiText.Fuzzy.getLocal() );
+                    }
+                    else
+                    {
+                        lines.add( GuiText.Partitioned.getLocal() + " - " + list + ' ' + GuiText.Precise.getLocal() );
+                    }
+                }
+            }
+        }
+    }
+
+    @Override
+    public int getBytes(final ItemStack cellItem)
+    {
+        return this.totalBytes;
+    }
+
+    @Override
+    public int BytePerType( final ItemStack cell )
+    {
+        return this.perType;
+    }
+
+    @Override
+    public int getBytesPerType( final ItemStack cellItem )
+    {
+        return this.perType;
+    }
+
+    @Override
+    public int getTotalTypes( final ItemStack cellItem )
+    {
         return 8;
     }
 
     @Override
-    public int BytePerType(ItemStack cellItem) {
-        return 8;
-    }
-
-    @Override
-    public int getBytesPerType(ItemStack cellItem) {
-        return 8;
-    }
-
-    @Override
-    public int getTotalTypes(ItemStack cellItem) {
-        return 1;
-    }
-
-    @Override
-    public boolean isBlackListed(ItemStack cellItem, IAEItemStack requestedAddition) {
+    public boolean isBlackListed( final ItemStack cellItem, final IAEItemStack requestedAddition )
+    {
         return false;
     }
 
     @Override
-    public boolean storableInStorageCell() {
+    public boolean storableInStorageCell()
+    {
         return false;
     }
 
     @Override
-    public boolean isStorageCell(ItemStack i) {
+    public boolean isStorageCell( final ItemStack i )
+    {
         return true;
     }
 
     @Override
-    public double getIdleDrain() {
-        return 0;
+    public double getIdleDrain()
+    {
+        return this.idleDrain;
     }
 
     @Override
-    public boolean isEditable(ItemStack is) {
+    public boolean isEditable( final ItemStack is )
+    {
+        return true;
+    }
+
+    @Override
+    public IInventory getUpgradesInventory( final ItemStack is )
+    {
+        return new CellUpgrades( is, 2 );
+    }
+
+    @Override
+    public IInventory getConfigInventory( final ItemStack is )
+    {
+        return new CellConfig( is );
+    }
+
+    @Override
+    public FuzzyMode getFuzzyMode( final ItemStack is )
+    {
+        final String fz = Platform.openNbtData( is ).getString( "FuzzyMode" );
+        try
+        {
+            return FuzzyMode.valueOf( fz );
+        }
+        catch( final Throwable t )
+        {
+            return FuzzyMode.IGNORE_ALL;
+        }
+    }
+
+    @Override
+    public void setFuzzyMode( final ItemStack is, final FuzzyMode fzMode )
+    {
+        Platform.openNbtData( is ).setString( "FuzzyMode", fzMode.name() );
+    }
+
+    @Override
+    public ItemStack onItemRightClick( final ItemStack stack, final World world, final EntityPlayer player )
+    {
+        this.disassembleDrive( stack, world, player );
+        return stack;
+    }
+
+    private boolean disassembleDrive(final ItemStack stack, final World world, final EntityPlayer player )
+    {
+        if( player.isSneaking() )
+        {
+            if( Platform.isClient() )
+            {
+                return false;
+            }
+
+            final InventoryPlayer playerInventory = player.inventory;
+            final IMEInventoryHandler inv = AEApi.instance().registries().cell().getCellInventory( stack, null, StorageChannel.ITEMS );
+            if( inv != null && playerInventory.getCurrentItem() == stack )
+            {
+                final InventoryAdaptor ia = InventoryAdaptor.getAdaptor( player, ForgeDirection.UNKNOWN );
+                final IItemList<IAEItemStack> list = inv.getAvailableItems( StorageChannel.ITEMS.createList() );
+                if( list.isEmpty() && ia != null )
+                {
+                    playerInventory.setInventorySlotContents( playerInventory.currentItem, null );
+
+                    // drop upgrades
+                    final IInventory upgradesInventory = this.getUpgradesInventory( stack );
+                    for( int upgradeIndex = 0; upgradeIndex < upgradesInventory.getSizeInventory(); upgradeIndex++ )
+                    {
+                        final ItemStack upgradeStack = upgradesInventory.getStackInSlot( upgradeIndex );
+                        final ItemStack leftStack = ia.addItems( upgradeStack );
+                        if( leftStack != null && upgradeStack.getItem() instanceof IUpgradeModule)
+                        {
+                            player.dropPlayerItemWithRandomChoice( upgradeStack, false );
+                        }
+                    }
+
+                    // drop empty storage cell case
+                    for( final ItemStack storageCellStack : AEApi.instance().definitions().materials().emptyStorageCell().maybeStack( 1 ).asSet() )
+                    {
+                        final ItemStack extraA = ia.addItems( storageCellStack );
+                        if( extraA != null )
+                        {
+                            player.dropPlayerItemWithRandomChoice( extraA, false );
+                        }
+                    }
+
+                    if( player.inventoryContainer != null )
+                    {
+                        player.inventoryContainer.detectAndSendChanges();
+                    }
+
+                    return true;
+                }
+            }
+        }
         return false;
     }
 
     @Override
-    public IInventory getUpgradesInventory(ItemStack is) {
-        return null;
+    public boolean onItemUseFirst( final ItemStack stack, final EntityPlayer player, final World world, final int x, final int y, final int z, final int side, final float hitX, final float hitY, final float hitZ )
+    {
+        return this.disassembleDrive( stack, world, player );
     }
 
     @Override
-    public IInventory getConfigInventory(ItemStack is) {
-        return null;
+    public ItemStack getContainerItem( final ItemStack itemStack )
+    {
+        for( final ItemStack stack : AEApi.instance().definitions().materials().emptyStorageCell().maybeStack( 1 ).asSet() )
+        {
+            return stack;
+        }
+
+        throw new MissingDefinition( "Tried to use empty storage cells while basic storage cells are defined." );
     }
 
     @Override
-    public FuzzyMode getFuzzyMode(ItemStack is) {
-        return null;
-    }
-
-    @Override
-    public void setFuzzyMode(ItemStack is, FuzzyMode fzMode) {
-
-    }
-
-    @Override
-    public double injectAEPower(ItemStack is, double amt) {
-        return 0;
-    }
-
-    @Override
-    public double extractAEPower(ItemStack is, double amt) {
-        return 0;
-    }
-
-    @Override
-    public double getAEMaxPower(ItemStack is) {
-        return 0;
-    }
-
-    @Override
-    public double getAECurrentPower(ItemStack is) {
-        return 0;
-    }
-
-    @Override
-    public AccessRestriction getPowerFlow(ItemStack is) {
-        return null;
+    public boolean hasContainerItem( final ItemStack stack )
+    {
+        return false;
     }
 }
